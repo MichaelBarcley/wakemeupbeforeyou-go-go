a83c0604dab5ed5656c2769b551e6f4a611c249b
Mihály Barczi <barczi.mihaly@gmail.com>
2019-05-20 16:19:56 +0200

    refact(main): remove testHandlers, reorder code

--- main.go
+++ main.go
@@ -15,28 +15,10 @@ var tokenCreationDate = time.Now()
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/token/", tokenHandler)
-	http.HandleFunc("/test/", testHandler)
-	http.HandleFunc("/test2/", test2Handler)
 	http.HandleFunc("/series/live", liveSeriesHandler)
 	http.HandleFunc("/players/live", livePlayersHandler)
 	http.HandleFunc("/teams/live", liveTeamsHandler)
 	http.ListenAndServe(":8080", nil)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Abios Gaming test exercise web application!")
-}
-
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	var lofasz = time.Now().Sub(tokenCreationDate) / 10e8
-	fmt.Fprintf(w, "Difference in seconds: %d", lofasz)
-}
-
-func test2Handler(w http.ResponseWriter, r *http.Request) {
-	var timeSinceTokenCreation = time.Now().Sub(tokenCreationDate) / 10e8
-	if timeSinceTokenCreation > 3600 || accessToken == "" {
-		getToken()
-	}
 }
 
 func tokenHandler(w http.ResponseWriter, r *http.Request) {
@@ -81,6 +63,10 @@ func checkIfTokenIsValid() {
 	if timeSinceTokenCreation > 3600 || accessToken == "" {
 		getToken()
 	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Abios Gaming test exercise web application!")
 }
 
 func liveSeriesHandler(w http.ResponseWriter, r *http.Request) {

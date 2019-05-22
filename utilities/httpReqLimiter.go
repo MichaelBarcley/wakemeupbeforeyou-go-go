61464e7163d29efb80e0bad4f4f9f76d78c75481
Mihály Barczi <barczi.mihaly@gmail.com>
2019-05-22 12:10:10 +0200

    feat(httpReqLimiter): impl limiter for each user based on IP

--- utilities/httpReqLimiter.go
+++ utilities/httpReqLimiter.go
@@ -1,18 +1,68 @@
 package utilities
 
 import (
+	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
 
-// Limiter ...
-var Limiter = rate.NewLimiter(1, 4)
+// Custom struct that holds the rate limiter for each visitor and the last time that the visitor was seen.
+type visitor struct {
+	limiter  *rate.Limiter
+	lastSeen time.Time
+}
 
-// Limit ...
+var visitors = make(map[string]*visitor)
+var mtx sync.Mutex
+
+// Run a background goroutine to remove old entries from the visitors map.
+func init() {
+	go cleanupVisitors()
+}
+
+func addVisitor(ip string) *rate.Limiter {
+	limiter := rate.NewLimiter(0.5, 5)
+	mtx.Lock()
+	visitors[ip] = &visitor{limiter, time.Now()}
+	mtx.Unlock()
+	return limiter
+}
+
+func getVisitor(ip string) *rate.Limiter {
+	mtx.Lock()
+	v, exists := visitors[ip]
+	if !exists {
+		mtx.Unlock()
+		return addVisitor(ip)
+	}
+
+	v.lastSeen = time.Now()
+	mtx.Unlock()
+	return v.limiter
+}
+
+func cleanupVisitors() {
+	for {
+		time.Sleep(time.Minute)
+		mtx.Lock()
+		for ip, v := range visitors {
+			if time.Now().Sub(v.lastSeen) > 3*time.Minute {
+				delete(visitors, ip)
+			}
+		}
+		mtx.Unlock()
+	}
+}
+
+// Limit checks if the user exceeded that limiter and return Status Code 429 in that case, or serves the request if not.
 func Limit(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if Limiter.Allow() == false {
+		limiter := getVisitor(r.RemoteAddr)
+		fmt.Println(r.Method + " request from IP: " + r.RemoteAddr + " to endpoint " + r.RequestURI)
+		if limiter.Allow() == false {
 			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
 			return
 		}
